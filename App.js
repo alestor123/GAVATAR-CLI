222e4545eb96aef5a0c811ae5479e62037d648c5
alestor123 <alestoraldous@gmail.com>
2021-01-08 13:05:27 +0530

    added main codee

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+
+var axios = require('axios'),
+fs = require('fs');
+(async () => {
+var data = await axios.get(`http://en.gravatar.com/${process.argv[2] || 'github'}.json`)
+console.log('Username : '+ data.data.entry[0].preferredUsername + ', Name : ' + data.data.entry[0].displayName )
+image = await axios.get(data.data.entry[0].thumbnailUrl, {responseType: 'arraybuffer'})
+fs.writeFileSync(`./${data.data.entry[0].preferredUsername}.png`, image.data, (err) => {
+    if (err) throw err;
+})
+console.log(`Downloaded image => ${data.data.entry[0].preferredUsername}.png `)
+})();
